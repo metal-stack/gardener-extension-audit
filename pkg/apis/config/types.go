53c11d297d207c94acdd3613a3d5fc3ed33c2e7d
Gerrit91 <info@gerritschwerthelm.de>
2023-11-14 12:58:20 +0100

    Allow operators to configure default backends.

--- pkg/apis/config/types.go
+++ pkg/apis/config/types.go
@@ -4,6 +4,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	healthcheckconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
+	"github.com/metal-stack/gardener-extension-audit/pkg/apis/audit/v1alpha1"
 )
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -12,31 +13,9 @@ import (
 type ControllerConfiguration struct {
 	metav1.TypeMeta
 
-	// // ClusterAudit is the configuration for cluster auditing.
-	// ClusterAudit ClusterAudit
-
-	// // AuditToSplunk is the configuration for forwarding audit (and firewall) logs to Splunk.
-	// AuditToSplunk AuditToSplunk
+	// DefaultBackends can be used to configure provider-default backends that are not explicitly disabled from the user.
+	DefaultBackends *v1alpha1.AuditBackends
 
 	// HealthCheckConfig is the config for the health check controller
 	HealthCheckConfig *healthcheckconfig.HealthCheckConfig
 }
-
-// // ClusterAudit is the configuration for cluster auditing.
-// type ClusterAudit struct {
-// 	// Enabled enables collecting of the kube-apiserver auditlog.
-// 	Enabled bool
-// }
-
-// // AuditToSplunk is the configuration for forwarding audit (and firewall) logs to Splunk.
-// type AuditToSplunk struct {
-// 	// Enabled enables forwarding of the kube-apiserver auditlog to splunk.
-// 	Enabled bool
-// 	// This defines the default splunk endpoint unless otherwise specified by the cluster user
-// 	HECToken   string
-// 	Index      string
-// 	HECHost    string
-// 	HECPort    int
-// 	TLSEnabled bool
-// 	HECCAFile  string
-// }
