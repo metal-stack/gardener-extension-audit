1c3caa7c780d949a3ce9005b6fbb4dc342eac905
relusc <rene.schach_ext@external.mail.schwarz>
2026-03-06 08:34:42 +0100

    Add nil check in validation

--- pkg/apis/audit/v1alpha1/validate.go
+++ pkg/apis/audit/v1alpha1/validate.go
@@ -5,7 +5,7 @@ import "fmt"
 // Validate performs checks against values of the submitted AuditConfig
 func (a *AuditConfig) Validate() error {
 	// Check that maximum log event size does not exceed limit
-	if *(a.Messages.MaxEventSize) < 0 || *(a.Messages.MaxEventSize) > AuditLogMaximumSizeEvent {
+	if a.Messages.MaxEventSize != nil && (*a.Messages.MaxEventSize < 0 || *a.Messages.MaxEventSize > AuditLogMaximumSizeEvent) {
 		return fmt.Errorf("messages.maxEventSize has to be greater than 0 and lower than %d", AuditLogMaximumSizeEvent)
 	}
 
