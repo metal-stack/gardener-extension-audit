5e02040d3e8caeca1bf43de12b081604f6ffae8c
Gerrit <tirreg91@googlemail.com>
2026-03-16 09:49:54 +0100

    Fix nil pointer.

--- pkg/apis/audit/v1alpha1/validate.go
+++ pkg/apis/audit/v1alpha1/validate.go
@@ -5,8 +5,10 @@ import "fmt"
 // Validate performs checks against values of the submitted AuditConfig
 func (a *AuditConfig) Validate() error {
 	// Check that maximum log event size does not exceed limit and minimum
-	if a.Messages.MaxEventSize != nil && (*a.Messages.MaxEventSize <= AuditLogEventBuffer || *a.Messages.MaxEventSize > AuditLogMaximumSizeEvent) {
-		return fmt.Errorf("messages.maxEventSize has to be greater than %d and lower than %d", AuditLogEventBuffer, AuditLogMaximumSizeEvent)
+	if a.Messages != nil && a.Messages.MaxEventSize != nil {
+		if *a.Messages.MaxEventSize <= AuditLogEventBuffer || *a.Messages.MaxEventSize > AuditLogMaximumSizeEvent {
+			return fmt.Errorf("messages.maxEventSize has to be greater than %d and lower than %d", AuditLogEventBuffer, AuditLogMaximumSizeEvent)
+		}
 	}
 
 	return nil
